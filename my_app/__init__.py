be84756f43b8e3fddf2d26e6d18ea333f4f4a057
Demondre Livingston <livingstondemondre7@gmail.com>
2018-05-02 20:08:58 -0400

    changes

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -12,16 +12,18 @@ def hello_world():
 def sum_num(num1, num2):
     return num1 + num2
 
+
 def subtract_num(num1, num2):
     return num1 - num2
 
 
-'''
-def multiply_num()
+def multiply_num(num1, num2):
+    return num1 / num2
 
 
-def divide_num()
-'''
+def divide_num(num1, num2):
+    return num1 - num2
+
 
 def prepare_response(message):
     # build response body to return back to Dialogflow
@@ -38,9 +40,10 @@ def prepare_response(message):
     print(message)
     return response
 
+
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
-    message = '' # going to store message for Chatbot to say
+    message = ''  # going to store message for Chatbot to say
 
     # convert request from Dialogflow to JSON
     req = request.get_json(silent=True, force=True)
@@ -49,18 +52,44 @@ def chatbot():
     # dissect the incoming message from dialogflow
     try:
         # if action is 'add_numbers' do addition
-        if (req['result']['action'] == 'add_numbers'):
+        if req['result']['action'] == 'add_numbers':
             # grab two numbers from dialogflow parameters
             num1 = int(req['result']['parameters']['num1'])
             num2 = int(req['result']['parameters']['num2'])
             # use sum_num() function to find sum of two numbers
             message = "The sum of {} and  {} is {}".format(num1, num2, sum_num(num1, num2))
             return prepare_response(message)
+
+        elif req['result']['action'] == 'subtract_numbers':
+            # grab two numbers from dialogflow parameters
+            num1 = int(req['result']['parameters']['num1'])
+            num2 = int(req['result']['parameters']['num2'])
+            # use sum_num() function to find sum of two numbers
+            message = "The sum of {} and  {} is {}".format(num1, num2, subtract_num(num1, num2))
+            return prepare_response(message)
+
+        elif req['result']['action'] == 'multiply_numbers':
+            # grab two numbers from dialogflow parameters
+            num1 = int(req['result']['parameters']['num1'])
+            num2 = int(req['result']['parameters']['num2'])
+            # use sum_num() function to find sum of two numbers
+            message = "The sum of {} and  {} is {}".format(num1, num2, multiply_num(num1, num2))
+            return prepare_response(message)
+
+        elif req['result']['action'] == 'divide_numbers':
+            # grab two numbers from dialogflow parameters
+            num1 = int(req['result']['parameters']['num1'])
+            num2 = int(req['result']['parameters']['num2'])
+            # use sum_num() function to find sum of two numbers
+            message = "The sum of {} and  {} is {}".format(num1, num2, divide_num(num1, num2))
+            return prepare_response(message)
+
+
+
     except:
         message = "Oops there was an error, try again"
         return prepare_response(message)
 
 
 if __name__ == '__main__':
-    app.run() # kick off our API here
-
+    app.run()  # kick off our API here
