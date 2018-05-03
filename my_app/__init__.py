b02a864e79f2a1f71851db41bc40769ee8337e6b
Demondre Livingston <livingstondemondre7@gmail.com>
2018-05-02 20:19:48 -0400

    fixed and added mulitply and divide and subtract

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -18,11 +18,11 @@ def subtract_num(num1, num2):
 
 
 def multiply_num(num1, num2):
-    return num1 / num2
+    return num1 * num2
 
 
 def divide_num(num1, num2):
-    return num1 - num2
+    return num1 / num2
 
 
 def prepare_response(message):
